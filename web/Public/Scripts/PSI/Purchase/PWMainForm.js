637261e2160d3a7f900b0987b6422ecffd5b33ce
sosungs <sosungs@foxmail.com>
2018-05-21 18:48:31 +0800

    采购单据列表字段添加

--- web/Public/Scripts/PSI/Purchase/PWMainForm.js
+++ web/Public/Scripts/PSI/Purchase/PWMainForm.js
@@ -478,12 +478,24 @@ Ext.define("PSI.Purchase.PWMainForm", {
 								sortable : false,
 								width : 200
 							}, {
+								header : "品牌",
+								dataIndex : "brandName",
+								menuDisabled : true,
+								sortable : false,
+								width : 100
+							},{
 								header : "规格型号",
 								dataIndex : "goodsSpec",
 								menuDisabled : true,
 								sortable : false,
 								width : 200
 							}, {
+								header : "尺寸",
+								dataIndex : "Chicun",
+								menuDisabled : true,
+								sortable : false,
+								width : 110
+							},{
 								header : "入库数量",
 								width : 120,
 								dataIndex : "goodsCount",
