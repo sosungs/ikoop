b4f8152249c2085e8ee4a1fd7fbe13365e74260e
sosungs <sosungs@foxmail.com>
2018-05-23 22:44:19 +0800

    更新字段列表宽度

--- web/Public/Scripts/PSI/Goods/GoodsWithPurchasePriceField.js
+++ web/Public/Scripts/PSI/Goods/GoodsWithPurchasePriceField.js
@@ -64,31 +64,26 @@ Ext.define("PSI.Goods.GoodsWithPurchaseFieldField", {
 								header : "商品",
 								dataIndex : "name",
 								menuDisabled : true,
-								flex : 1,
 								width : 150
 							}, {
 								header : "品牌",
 								dataIndex : "brandName",
 								menuDisabled : true,
-								flex : 1,
 								width : 80
 							}, {
 								header : "规格型号",
 								dataIndex : "spec",
 								menuDisabled : true,
-								flex : 1,
 								width : 200
 							}, {
 								header : "老代号",
 								dataIndex : "oldSpec",
 								menuDisabled : true,
-								flex : 1,
 								width : 150
 							}, {
 								header : "尺寸",
 								dataIndex : "chiCun",
 								menuDisabled : true,
-								flex : 1,
 								width : 100
 							}, {
 								header : "单位",
